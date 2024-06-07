0149af4612e6cf118e8b15256a3bd9c8626b97e1
storm0611 <devstar0611@gmail.com>
2024-06-07 11:31:50 +0200

    simple contract

---
+++ contract/src/lib.rs
@@ -0,0 +1,33 @@
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+    msg,
+};
+
+entrypoint!(process_instruction);
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8]
+) -> ProgramResult {
+    msg!("Hello, world!");
+    Ok(())
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
